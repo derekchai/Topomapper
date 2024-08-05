7d6030552bf2b5663012429e99718f990260b795
Derek Chai <chderek9@gmail.com>
2024-08-05 17:44:00 +1200

    Add FeatureCollection to represent a GeoJSON FeatureCollection

---
+++ Topomapper/Models/FeatureCollection.swift
@@ -0,0 +1,47 @@
+//
+//  FeatureCollection.swift
+//  Topomapper
+//
+//  Created by Derek Chai on 05/08/2024.
+//
+
+import Foundation
+import CoreLocation
+
+/// A model representing certain properties of a GeoJSON `FeatureCollection`
+/// relevant to ``Topomapper``.
+///
+/// For more information about the GeoJSON standard, see the [GeoJSON Specification
+/// (RFC 7946)](https://datatracker.ietf.org/doc/html/rfc7946).
+struct FeatureCollection: Codable {
+    let features: [Feature]
+    
+    struct Feature: Codable {
+        let geometry: Geometry
+        let properties: Properties
+        
+        struct Geometry: Codable {
+            let coordinates: [Coordinates]
+            
+            struct Coordinates: Codable {
+                let longitude: Double
+                let latitude: Double
+                let elevation: CLLocationDistance
+            }
+        }
+        
+        struct Properties: Codable {
+            let segments: [Segment]
+            
+            struct Segment: Codable {
+                let steps: [Step]
+                
+                struct Step: Codable {
+                    let distance: CLLocationDistance
+                    let instruction: String
+                    let name: String
+                }
+            }
+        }
+    }
+}
