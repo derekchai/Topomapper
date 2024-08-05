3ed88e3daa4bd0d59d4a9b9f267d50e865539eb4
Derek Chai <chderek9@gmail.com>
2024-08-05 13:16:53 +1200

    Add Grade (a.k.a. Double) typealias

---
+++ Topomapper/Models/Waypoint.swift
@@ -0,0 +1,8 @@
+//
+//  Waypoint.swift
+//  Topomapper
+//
+//  Created by Derek Chai on 05/08/2024.
+//
+
+import Foundation
---
+++ Topomapper/Type Aliases/TypeAliases.swift
@@ -0,0 +1,10 @@
+//
+//  TypeAliases.swift
+//  Topomapper
+//
+//  Created by Derek Chai on 05/08/2024.
+//
+
+import Foundation
+
+typealias Grade = Double
