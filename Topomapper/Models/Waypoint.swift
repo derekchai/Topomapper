025623f482ff22a6346134aa2e203b9152413459
Derek Chai <chderek9@gmail.com>
2024-08-05 13:21:58 +1200

    Add Waypoint struct

--- Topomapper/Models/Waypoint.swift
+++ Topomapper/Models/Waypoint.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import CoreLocation
+
+/// A model representing a point on a ``Route``.
+struct Waypoint: Codable {
+    /// The coordinate of this ``Waypoint``.
+    var coordinate: CLLocationCoordinate2D
+    
+    /// The grade (percentage gradient) at this ``Waypoint`` on a ``Route``.
+    var grade: Grade
+    
+    /// The elevation of this ``Waypoint``.
+    var elevation: CLLocationDistance
+    
+    /// The distance from the start of the ``Route`` this ``Waypoint`` is located
+    /// at.
+    var distanceFromStart: CLLocationDistance
+}
