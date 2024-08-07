538378818dfa80fff0d04cbb0d98e5d7ada5f46f
Derek Chai <chderek9@gmail.com>
2024-08-07 13:17:49 +1200

    Add preview model container

---
+++ Topomapper/Preview Content/DataController.swift
@@ -0,0 +1,33 @@
+//
+//  DataController.swift
+//  Topomapper
+//
+//  Created by Derek Chai on 07/08/2024.
+//
+
+import Foundation
+import SwiftData
+
+@MainActor
+class DataController {
+    static let previewModelContainer: ModelContainer = {
+        let schema = Schema([
+            Route.self
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
+        
+        do {
+            let container = try ModelContainer(for: schema, configurations: [modelConfiguration])
+            
+            for i in 1...10 {
+                let route = Route(name: "Route \(i)", creationDate: Date(), waypoints: [])
+                
+                container.mainContext.insert(route)
+            }
+            
+            return container
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+}
