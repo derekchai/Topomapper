9dd11ac8727deec05f6b46d5129b945b320b67a3
Derek Chai <chderek9@gmail.com>
2024-08-08 12:10:46 +1200

    Add confirmation dialog to Route deletion

--- Topomapper/User Interface/Sidebar.swift
+++ Topomapper/User Interface/Sidebar.swift
@@ -15,6 +15,7 @@ struct Sidebar: View {
     // MARK: - Internal Properties
     @Environment(\.modelContext) private var modelContext
     @Query(sort: \Route.creationDate) private var userRoutes: [Route]
+    @State private var isShowingDeleteRouteAlert: Bool = false
     
     // MARK: - Body
     var body: some View {
@@ -24,19 +25,28 @@ struct Sidebar: View {
                     NavigationLink(value: route) {
                         TextField("Route name", text: Bindable(route).name)
                     }
+                    .contextMenu {
+                        Button(
+                            "Delete Route",
+                            role: .destructive,
+                            action: showDeleteRouteAlert
+                        )
+                    }
+                    .confirmationDialog(
+                        "Are you sure you want to delete this Route?",
+                        isPresented: $isShowingDeleteRouteAlert) {
+                            Button(
+                                "Delete",
+                                role: .destructive,
+                                action: {deleteRoute(route)
+                                })
+                        } message: {
+                            Text("This action is irreversible.")
+                        }
                 }
             }
         }
         .toolbar {
-            ToolbarItem(placement: .destructiveAction) {
-                Button(
-                    "Delete selected Route",
-                    systemImage: "trash",
-                    role: .destructive,
-                    action: {
-                    })
-                .disabled(selectedRoute == nil)
-            }
             ToolbarItem(placement: .primaryAction) {
                 Button("New Route", systemImage: "plus", action: createNewRoute)
             }
@@ -61,6 +71,26 @@ extension Sidebar {
         )
         modelContext.insert(route)
     }
+    
+    /// Deletes the currently selected ``Route`` from the model context.
+    private func deleteSelectedRoute() {
+        if let selectedRoute {
+            modelContext.delete(selectedRoute)
+        }
+    }
+    
+    /// Deletes the given ``Route`` from the model context.
+    /// - Parameter route: The ``Route`` to delete.
+    private func deleteRoute(_ route: Route) {
+        withAnimation {
+            modelContext.delete(route)
+        }
+    }
+    
+    /// Show the ``Route`` deletion confirmation alert.
+    private func showDeleteRouteAlert() {
+        isShowingDeleteRouteAlert = true
+    }
 }
 
 // MARK: - Preview
@@ -68,5 +98,5 @@ extension Sidebar {
     Sidebar(
         selectedRoute: .constant(Route(name: "My ROute", creationDate: Date(), waypoints: []))
     )
-        .modelContainer(DataController.previewModelContainer)
+    .modelContainer(DataController.previewModelContainer)
 }
