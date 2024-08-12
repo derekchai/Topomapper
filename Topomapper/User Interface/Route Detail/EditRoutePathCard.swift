017a1cd59f8a73b2872ac42252c7894e9fb6e4e9
Derek Chai <chderek9@gmail.com>
2024-08-12 13:42:51 +1200

    Add reorderable list to EditRoutePathCard

--- Topomapper/User Interface/Route Detail/EditRoutePathCard.swift
+++ Topomapper/User Interface/Route Detail/EditRoutePathCard.swift
@@ -12,7 +12,11 @@ struct EditRoutePathCard: View {
     // MARK: - Exposed Properties
     @Binding var route: Route
     
+    /// This closure is called when the 'Close' button of the view is pressed.
     var onDidPressCloseButton = {}
+    
+    // MARK: - Private Properties
+    @State private var stops = ["Stop A", "Stop B", "Stop C"]
     
     // MARK: - Body
     var body: some View {
@@ -23,12 +27,47 @@ struct EditRoutePathCard: View {
                 }
                 .buttonStyle(.plain)
             
+                Spacer()
+                
                 Text("Edit Route path")
                     .font(.headline)
+            } // HStack
+            
+            List {
+                ForEach($stops, id: \.self, editActions: .all) { stop in
+                    HStack {
+                        Image(systemName: "mappin.circle.fill")
+                            .foregroundStyle(.tertiary)
+                        
+                        Text(stop.wrappedValue)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "line.3.horizontal")
+                            .foregroundStyle(.tertiary)
+                    }
+                    .listRowSeparatorTint(.secondary.opacity(0.3))
+                }
+                
+                HStack {
+                    Image(systemName: "plus.circle.fill")
+                        .foregroundStyle(.blue)
+                    
+                    Text("Tap on the map to add a stop")
+                        .foregroundStyle(.tertiary)
+                    
+                    Spacer()
+                }
+                .listRowSeparatorTint(.secondary.opacity(0.3))
             }
+            .background(.quinary)
+            .scrollContentBackground(.hidden)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            .padding(.top)
         }
+            .frame(maxWidth: 200)
             .padding()  // Interior padding
-            .background(.ultraThickMaterial)
+            .background(.thickMaterial)
             .clipShape(RoundedRectangle(cornerRadius: 16))
             .shadow(radius: 16)
             .padding()  // Exterior padding
@@ -39,5 +78,12 @@ struct EditRoutePathCard: View {
 
 // MARK: - Preview
 #Preview {
-    EditRoutePathCard(route: .constant(Route.angelusHut))
+    let route = Route(
+        name: "My Route",
+        creationDate: Date(),
+        userDescription: "My Route description",
+        waypoints: []
+    )
+    
+    EditRoutePathCard(route: .constant(route))
 }
