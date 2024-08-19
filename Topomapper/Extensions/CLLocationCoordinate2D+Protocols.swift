eed550d09165ed87131f2c0b3d5285cfd080b46f
Derek Chai <chderek9@gmail.com>
2024-08-19 15:50:24 +1200

    Conform CLLocationCoordinate2D to Equatable, Hashable

--- Topomapper/Extensions/CLLocationCoordinate2D+Protocols.swift
+++ Topomapper/Extensions/CLLocationCoordinate2D+Protocols.swift
@@ -30,3 +30,20 @@ extension CLLocationCoordinate2D: Codable {
         self.init(latitude: latitude, longitude: longitude)
     }
 }
+
+extension CLLocationCoordinate2D: @retroactive Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(latitude)
+        hasher.combine(longitude)
+    }
+}
+
+extension CLLocationCoordinate2D: @retroactive Equatable {
+    public static func == (
+        lhs: CLLocationCoordinate2D,
+        rhs: CLLocationCoordinate2D
+    ) -> Bool {
+        return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
+    }
+}
+
